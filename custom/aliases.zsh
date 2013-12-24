143fbe158111b8e613c621bb76b419bb943d0794
Aaron Fraser <aaronmfraser@gmail.com>
2013-12-24 13:52:31 -0800

    Adding aliases.sh

---
+++ custom/aliases.zsh
@@ -0,0 +1,11 @@
+alias keepass-checkout="scp infra-bastion01.compliant.disney.private:/root/techops/TechOpsKeePassX.kdb ~/Documents/TechOpsKeePassX.kdb"
+alias keepass-checkin="scp ~/Documents/TechOpsKeePassX.kdb infra-bastion01.compliant.disney.private:/root/techops/TechOpsKeePassX.kdb"
+alias infra-compliant="ssh infra-bastion01.compliant.disney.private"
+alias infra-general="ssh infra-bastion01.general.disney.private"
+alias nops="ssh nops.pd.starwave.com -p 2020"
+alias csg1='ssh n7pg01dimg001dtbs001.general.disney.private'
+alias csg2='ssh n7pg01dimg001dtbs002.general.disney.private'
+alias csc1='ssh n7ps01dimg001dtbs001.compliant.disney.private'
+alias csc2='ssh n7ps01dimg001dtbs002.compliant.disney.private'
+alias razor="/Users/afraser/projects/personal/razor-client/bin/razor -u http://localhost:18081/api"
+alias docker="ssh afraser@docker.afraser.io"
