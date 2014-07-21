193042e0b3772d0332c37801558314b3d077f5f8
Aaron Fraser <aaronmfraser@gmail.com>
2014-07-21 09:35:04 -0700

    Updating aliases

---
+++ custom/env.zsh
@@ -0,0 +1,7 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+
+export RAZOR_API=http://localhost:18081/api
