6a7abd5ca1f316cc2028ad58dc73a26a801141f6
Aaron Fraser <aaronmfraser@gmail.com>
2014-07-22 21:34:31 -0700

    updates to aliases.zsh

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,7 +2,6 @@ alias keepass-checkout="scp infra-bastion01.compliant.disney.private:/root/techo
 alias keepass-checkin="scp ~/Documents/TechOpsKeePassX.kdb infra-bastion01.compliant.disney.private:/root/techops/TechOpsKeePassX.kdb"
 alias infra-compliant="ssh infra-bastion01.compliant.disney.private"
 alias infra-general="ssh infra-bastion01.general.disney.private"
-alias nops="ssh nops.pd.starwave.com -p 2020"
 alias csg1='ssh n7pg01dimg001dtbs001.general.disney.private'
 alias csg2='ssh n7pg01dimg001dtbs002.general.disney.private'
 alias csc1='ssh n7ps01dimg001dtbs001.compliant.disney.private'
