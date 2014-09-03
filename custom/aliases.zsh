722bb5040c6518a2439df59aa01a78efb7d79819
Aaron Fraser <aaronmfraser@gmail.com>
2014-09-02 23:30:33 -0700

    removing bad records from aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,7 +1,5 @@
 alias keepass-checkout="scp infra-bastion01.compliant.disney.private:/root/techops/TechOpsKeePassX.kdb ~/Documents/TechOpsKeePassX.kdb"
 alias keepass-checkin="scp ~/Documents/TechOpsKeePassX.kdb infra-bastion01.compliant.disney.private:/root/techops/TechOpsKeePassX.kdb"
-alias infra-compliant="ssh infra-bastion01.compliant.disney.private"
-alias infra-general="ssh infra-bastion01.general.disney.private"
 alias csg1='ssh n7pg01dimg001dtbs001.general.disney.private'
 alias csg2='ssh n7pg01dimg001dtbs002.general.disney.private'
 alias csc1='ssh n7ps01dimg001dtbs001.compliant.disney.private'
