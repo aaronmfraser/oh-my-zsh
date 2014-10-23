0d8f85478f57145590603f846289c3f1a1ae3f75
Aaron Fraser <aaronmfraser@gmail.com>
2014-10-23 12:17:52 -0700

    Updatings to custom env scripts

--- custom/env.zsh
+++ custom/env.zsh
@@ -4,6 +4,7 @@
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
 
+export EC2_AMITOOL_HOME=/usr/local/ec2/ec2-ami-tools-1.5.3
 export EC2_HOME=/usr/local/ec2/ec2-api-tools-1.7.1.1
 export RAZOR_API=http://localhost:18081/api
 export JAVA_HOME=$(/usr/libexec/java_home)
